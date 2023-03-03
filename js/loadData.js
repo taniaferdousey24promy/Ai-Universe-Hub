01f07acd51ec79de685fddea9a3a02e767a0f93e
taniaferdousey24promy <taniaferdousey24promy@gmail.com>
2023-03-03 21:20:37 +0600

    second commit to show 6 cards by default and see more button to see all 12 cards

--- js/loadData.js
+++ js/loadData.js
@@ -3,7 +3,10 @@ const loadData = fetchingData => {
     console.log(url);
     fetch(url)
     .then(res => res.json())
+    // .then (allData => console.log(allData))
     .then (allData => eachAiAssistants(allData.data.tools))
+
+
     // .then (allData => console.log(allData.data.tools[0].name))
 
 }
@@ -12,7 +15,7 @@ const loadData = fetchingData => {
 const eachAiAssistants = assistantDatas => {
     const aiCardContainer = document.getElementById('ai-card-container');
 
-    assistantDatas.forEach(assistantData => {
+    assistantDatas.slice(0,6).forEach(assistantData => {
         console.log(assistantData)
         const assistantDiv = document.createElement('div');
         assistantDiv.classList.add('col');
@@ -67,7 +70,92 @@ const eachAiAssistants = assistantDatas => {
 
     })
 
+    //----------------------if see more button gets clicked rest of the cards will be available to the users
+    const seeMoreButton = document.getElementById('see-more-button');
+    seeMoreButton.addEventListener('click', function(){
+        
+
+    
+
+
+    assistantDatas.slice(6,12).forEach(assistantData => {
+
+        const assistantDiv = document.createElement('div');
+        assistantDiv.classList.add('col');
+        assistantDiv.innerHTML=
+        `
+        <div class="card p-4 ">
+                    <img src="${assistantData.image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">${assistantData.name}</h5>
+                      <ol class="text-left" type="1">
+                        <li>${assistantData.features[0]}</li>
+                        <li>${assistantData.features[1]}</li>
+                        <li>${assistantData.features[2]}</li>
+
+                      </ol>
+                      
+                        
+
+                      
+
+                    
+                    </div>
+                    <div class="card-footer bg-body">
+                        <h5 class="card-title">${assistantData.name}</h5>
+                        <div class="d-flex justify-content-between">
+                            <div >
+                                <i class="fa-sharp fa-regular fa-calendar-days"></i>
+
+                            </div>
+
+                            <div class="flex-fill">
+                                ${assistantData.published_in}
+
+                            </div>
+
+                            <div>
+
+                                <button class="rounded-circle border-0 "><i class="fa-solid fa-arrow-right"></i></button>
+
+                            </div>
+
+                        </div>
+
+                        <!-- <input class="border-0" value="2017-06-01" type="date"  /> -->
+
+
+                    </div>
+                  </div>
+        `;
+        aiCardContainer.appendChild(assistantDiv);
+        
+
+
+    
+
+    })
+    this.style.display ='none';
+
+
+
+
+
+})
+
+
 }
+
+// const seeMoreButton = document.getElementById('see-more-button');
+// seeMoreButton.addEventListener('click', function(){
+
+//     eachAiAssistantsSeeMore();
+
+
+
+
+// })
+
 
 
 loadData();
